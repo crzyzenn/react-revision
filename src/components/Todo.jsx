a6d5fa9b3117e29c02b83157e9120d512d381f84
Zenish Shrestha <zenishshr@gmail.com>
2021-07-15 08:20:34 +0545

    Finish todos app.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { uuid } from "uuidv4";
 import TodoCard from "./TodoCard/Index";
 
@@ -24,19 +24,53 @@ export default function Todo() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
+  const [completedTodos, setCompletedTodos] = useState([]);
+  const [pendingTodos, setPendingTodos] = useState([]);
+
+  useEffect(() => {
+    console.log("Todo modified");
+    // Get the completed todos.
+    // ...
+    // const completed = todos.filter((todo) => {
+    //   if (todo.completed === true) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // });
+    const completed = todos.filter((todo) => todo.completed);
+    const notCompleted = todos.filter((todo) => !todo.completed);
+
+    setPendingTodos(notCompleted);
+    setCompletedTodos(completed);
+  }, [todos]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputValue) {
-      const taskToAdd = {
-        id: uuid(),
-        name: inputValue,
-        completed: false
-      };
-      // setTodos(todos.push(taskToAdd));
-      setTodos([...todos, taskToAdd]);
+      // Check duplicate
+      const foundIndex = todos.findIndex((todo) => {
+        if (todo.name == inputValue) {
+          return true;
+        } else {
+          return false;
+        }
+      });
 
-      // After task has been created reset input field
-      setInputValue("");
+      if (foundIndex === -1) {
+        const taskToAdd = {
+          id: uuid(),
+          name: inputValue,
+          completed: false
+        };
+        // setTodos(todos.push(taskToAdd));
+        setTodos([...todos, taskToAdd]);
+
+        // After task has been created reset input field
+        setInputValue("");
+      } else {
+        alert("Task already exists. Add a different one!");
+      }
     } else {
       alert("Please type something..");
     }
@@ -72,6 +106,53 @@ export default function Todo() {
     console.log("Checking the task ", id, modifiedTodos);
   };
 
+  const handleDelete = (id) => {
+    // todos = [
+    //   {
+    //     id: 'unique_id',
+    //     name: 'task name',
+    //     completed: false
+    //   },
+    //   {
+    //     id: 'unique_id',
+    //     name: 'task name',
+    //     completed: false
+    //   }
+    // ]
+
+    const modifiedTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(modifiedTodos);
+  };
+
+  const handleUndo = (id) => {
+    // todos = [
+    //   {
+    //     id: 'unique_id',
+    //     name: 'task name',
+    //     completed: false
+    //   },
+    //   {
+    //     id: 'unique_id',
+    //     name: 'task name',
+    //     completed: false
+    //   }
+    // ]
+
+    const modifiedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: false
+        };
+      } else {
+        return todo;
+      }
+    });
+
+    setTodos(modifiedTodos);
+  };
+
   return (
     <div>
       <h1>New Todo</h1>
@@ -100,15 +181,41 @@ export default function Todo() {
         }}
       >
         {/* Display todos */}
-        {todos.map((todo) => (
-          <TodoCard
-            key={todo.id}
-            id={todo.id}
-            name={todo.name}
-            completed={todo.completed}
-            onCheckClick={handleCheck}
-          />
-        ))}
+        {pendingTodos.length === 0
+          ? "No todos"
+          : pendingTodos.map((todo) => (
+              <TodoCard
+                key={todo.id}
+                id={todo.id}
+                name={todo.name}
+                completed={todo.completed}
+                onCheckClick={handleCheck}
+                onDeleteClick={handleDelete}
+              />
+            ))}
+      </div>
+      <h1>Completed Todos</h1>
+      <div
+        style={{
+          display: "flex",
+          gap: 10,
+          flexWrap: "wrap"
+        }}
+      >
+        {/* Display todos */}
+        {completedTodos.length === 0
+          ? "No todos"
+          : completedTodos.map((todo) => (
+              <TodoCard
+                key={todo.id}
+                id={todo.id}
+                name={todo.name}
+                completed={todo.completed}
+                onCheckClick={handleCheck}
+                onDeleteClick={handleDelete}
+                onUndoClick={handleUndo}
+              />
+            ))}
       </div>
     </div>
   );
--- src/components/TodoCard/Index.jsx
+++ src/components/TodoCard/Index.jsx
@@ -2,25 +2,49 @@ import React from "react";
 import "./todocard.css";
 import CheckIcon from "@material-ui/icons/Check";
 import DeleteIcon from "@material-ui/icons/Delete";
+import UndoIcon from "@material-ui/icons/Undo";
 
-export default function TodoCard({ id, name, onCheckClick, completed }) {
+export default function TodoCard({
+  id,
+  name,
+  onCheckClick,
+  completed,
+  onDeleteClick,
+  onUndoClick
+}) {
   return (
     <div className="todocard--container">
       <h3>{name}</h3>
       {completed ? "Completed" : "Not Completed"}
 
-      <div className="todocard--buttons">
-        <button
-          onClick={() => {
-            onCheckClick(id);
-          }}
-        >
-          <CheckIcon />
-        </button>
-        <button>
-          <DeleteIcon />
-        </button>
-      </div>
+      {completed ? (
+        <div className="todocard--buttons">
+          <button
+            onClick={() => {
+              onUndoClick(id);
+            }}
+          >
+            <UndoIcon />
+          </button>
+        </div>
+      ) : (
+        <div className="todocard--buttons">
+          <button
+            onClick={() => {
+              onCheckClick(id);
+            }}
+          >
+            <CheckIcon />
+          </button>
+          <button
+            onClick={() => {
+              onDeleteClick(id);
+            }}
+          >
+            <DeleteIcon />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
